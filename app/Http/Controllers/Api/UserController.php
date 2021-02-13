c140cfde981cb2b6806767f77b175fd0cb658561
Abdul Majid <abdulmajidcse@gmail.com>
2021-02-13 09:53:30 +0600

    created api users route & index, show and delete user.

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::latest('id')->get();
+        if($users->count() > 0) {
+            return response()->json($users, 200);
+        }
+        return response()->json('User not found', 404);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        if($user) {
+            return response()->json($user, 200);
+        }
+        return response()->json('User not found', 404);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        if(!$user) {
+            return response()->json('User not found', 404);
+        }
+
+        //delete photo
+        if($user->photo && file_exists('assets/uploads/'.$user->photo)) {
+            unlink('assets/uploads/'.$user->photo);
+        }
+
+        $user->delete();
+        return response()->json('User deleted', 200);
+    }
+}
